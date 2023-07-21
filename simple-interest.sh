a3e5a6b7e289955c30628d7486ea41568d147d7a
ajayyadav8741 <133377084+ajayyadav8741@users.noreply.github.com>
2023-07-21 19:26:21 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <ajayyadav8741>
    # Input:
    # p, principal amount
    # t, time period in years
